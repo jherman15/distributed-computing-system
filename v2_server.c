9e1a967f86982eeb9d339b3d00b55054ce5e50ad
root <root@debian-ps.agh.edu.pl>
2022-02-02 19:29:42 +0100

    done

--- v2_server.c
+++ v2_server.c
@@ -1,13 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <ctype.h>
-#include <arpa/inet.h>
+#include        <sys/types.h>   /* basic system data types */
+#include        <sys/socket.h>  /* basic socket definitions */
+#include        <sys/time.h>    /* timeval{} for select() */
+#include        <time.h>                /* timespec{} for pselect() */
+#include        <netinet/in.h>  /* sockaddr_in{} and other Internet defns */
+#include        <arpa/inet.h>   /* inet(3) functions */
+#include        <errno.h>
+#include        <fcntl.h>               /* for nonblocking */
+#include        <netdb.h>
+#include        <signal.h>
+#include        <stdio.h>
+#include        <stdlib.h>
+#include        <string.h>
+#include        <unistd.h>
+#include        <sys/wait.h>
+#include        <ctype.h>
 
+void
+sig_chld(int signo)                                                             //jh: SIGCHILD
+{
+        pid_t   pid;
+        int             stat;
+
+        while ( (pid = waitpid(-1, &stat, WNOHANG)) > 0)
+                printf("child %d terminated\n", pid);
+        return;
+}
 
 void error(const char *msg)
 {
@@ -23,10 +40,33 @@ int main(int argc, char *argv[])
      char buffer[1024];
      char buffer1[1024];
      struct sockaddr_in serv_addr, cli_addr;
+     void                            sig_chld(int);
 
      int n,ans;
      socklen_t addr_size;
      pid_t childpid;
+
+
+#define SIGCHLD_                                                                //jh: SIGCHILD
+#ifdef SIGCHLD
+    struct sigaction new_action, old_action;
+
+  /* Set up the structure to specify the new action. */
+    new_action.sa_handler = sig_chld;
+    new_action.sa_handler = SIG_IGN;
+    sigemptyset (&new_action.sa_mask);
+    new_action.sa_flags = 0;
+
+    if( sigaction (SIGCHLD, &new_action, &old_action) < 0 ){
+          fprintf(stderr,"sigaction error : %s\n", strerror(errno));
+          return 1;
+    }
+
+#endif
+      //signal(SIGCHLD, sig_chld);
+      signal(SIGCHLD, SIG_IGN);
+
+
 
      if (argc < 2) {
          fprintf(stderr,"ERROR, no port provided\n");
@@ -44,14 +84,6 @@ int main(int argc, char *argv[])
               sizeof(serv_addr)) < 0)
               error("ERROR on binding");
      listen(sockfd,5);                                          // The number gives the maximum limit of client at same time
-     //newsockfd = accept(sockfd,
-       //          (struct sockaddr *) &cli_addr,
-         //        &clilen);
-     //if (newsockfd < 0)
-       //   error("ERROR on accept");
-
-
- //int num1, num2, num3, num4, ans, ans2;
 
 
         while(1){
@@ -60,22 +92,6 @@ int main(int argc, char *argv[])
                         exit(1);
                 }
                 printf("Connection accepted from %s:%d\n", inet_ntoa(serv_addr.sin_addr), ntohs(serv_addr.sin_port));
-//@@@@@@@@@@@@@@@@@ CALCULATOR PART @@@@@@@@@@@@@@@@@@@@
-
-//     int num1, num2, num3, num4, ans, ans2;
-
-  //   printf("Enter number 1: ");                                        //first number (client2)
-    // scanf("%d", &num1);
-     //n = write(newsockfd, &num1, sizeof(int));
-    // if (n < 0) error("ERROR writing to socket");
-
-   //  printf("Enter number 2: ");                                        //second number (client2)
-   //  scanf("%d", &num2);
-   //  n = write(newsockfd, &num2, sizeof(int));
-   //  if (n < 0) error("ERROR writing to socket");
-
-  //   read(newsockfd, &ans, sizeof(int));
-  //   printf("Ans is:%d\n", ans);
 
 int num1,num2;
 
@@ -84,9 +100,7 @@ int num1,num2;
                         close(sockfd);
 
                         while(1){
-                                //recv(newsockfd, buffer, 1024, 0);
-                                //printf("Server: \t");
-
+ 
                                 printf("Enter number 1: ");
                                 scanf("%s", &buffer);
                                 send(newsockfd, &buffer, strlen(buffer), 0);
@@ -99,7 +113,6 @@ int num1,num2;
                                 n = write(newsockfd, &buffer1, sizeof(buffer1));
                                 if (n < 0) error("ERROR writing to socket");
 
-///                             send(newsockfd, buffer, strlen(buffer), 0);
                                 if(strcmp(buffer1, ":exit") == 0){
                                         printf("Disconnected from %s:%d\n", inet_ntoa(serv_addr.sin_addr), ntohs(serv_addr.sin_port));
                                         break;
@@ -117,21 +130,21 @@ int num1,num2;
                                                         printf("Client not responding\n");
                                                         close(newsockfd);
                                                 }
-                                                else{
-                                                
-                                                        printf("The result is: %d\n", ans);
-                                                }
-                                        }
+	                                        else{
+        	                                        printf("The result is: %d\n", ans);
+                	                        }//else
+                                        }//if(ans==20)
+					else{
+                                                 printf("The result is: %d\n", ans);
+                                        }//else
 
-                                }
+                                }//outer else
 
-
-
-                        }
-                }
-        }
+                        }// if(strcmp...)
+                }// inner while
+        }//outer while
 
      close(newsockfd);
      close(sockfd);
      return 0;
-}
+}//main
