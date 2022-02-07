12b25a266d9dc65e2da025e1e7f40b6de60e6df2
jherman15 <jherman@student.agh.edu.pl>
2022-02-07 19:29:31 +0100

    update of clients

--- v2_client1.c
+++ v2_client1.c
@@ -1,4 +1,5 @@
-//v2_client1.c
+/* Distributed computing system - client 1
+Bulanda Karol, Herman Jaroslaw, Rosol Jan*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,24 +18,29 @@ void error(const char *msg)
 }
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
+    int sockfd, portno, n, num1, num2, sum;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-    char buffer[1024];
+    char buffer[1024], buffer1[1024];
+
     if (argc < 3)
     {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
     }
+
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
     if (sockfd < 0)
         error("ERROR opening socket");
     server = gethostbyname(argv[1]);
+
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
+
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr,
@@ -44,45 +50,33 @@ int main(int argc, char *argv[])
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
         error("ERROR connecting");
 
-int suma;
-int num1, num2;
-char buffer1[1024];
 
         while(1){
-                //sleep(5);
-                n = recv(sockfd, &buffer, 1024, 0);                         //Read Server String (num3)
+                n = recv(sockfd, &buffer, 1024, 0);                         //receiving first digit from the server
                 if (n < 0)
                         error("ERROR reading from socket");
                 recv(sockfd, &buffer, 1024, 0);
-                printf("First number: %s\n", buffer);
-                n = recv(sockfd, &buffer1, 1024, 0);                         //Read Server String (num3)
+                printf("First digit: %s\n", buffer);
+
+                n = recv(sockfd, &buffer1, 1024, 0);                         //receiving second digit from the server
                 if (n < 0)
                         error("ERROR reading from socket");
                 recv(sockfd, &buffer1, 1024, 0);
-                printf("Second number: %s\n", buffer1);
-        //      printf("Client: \t");
-        //      scanf("%s", &buffer[0]);
-        //      send(sockfd, buffer, strlen(buffer), 0);
+                printf("Second digit: %s\n", buffer1);
 
-                if(strcmp(buffer, ":exit") == 0){
+                if(strcmp(buffer1, ":exit") == 0){
                         close(sockfd);
                         printf("[-]Disconnected from server.\n");
                         exit(1);
                 }
 
-                //for(int i=0; i<=1; i++){
-                       // n = recv(sockfd, buffer, 1024, 0);                         //Read Server String (num3)
-                       // if (n < 0)
-                       //      error("ERROR reading from socket");
-
                 num1 = *buffer - '0';
                 num2 = *buffer1 - '0';
-                suma = num1+num2;                       //Send ans2 to server
-                write(sockfd,&suma, sizeof(suma));
+                sum = num1+num2;                       //computing the result and sending it back to the server
+                write(sockfd,&sum, sizeof(sum));
          }
 
 close(sockfd);
 return 0;
 }
-
 
